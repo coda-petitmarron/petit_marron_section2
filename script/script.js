bddc96810dd38d51e7cef2c6245b4a363accdac0
samir bensadi <samirbensadi@gmail.com>
2016-10-03 16:17:59 +0200

    site bêta

--- script/script.js
+++ script/script.js
@@ -2,12 +2,25 @@
 
 $(document).ready(function(){
 
-	
+	$('.smallSection:not(#one)').hide();
 
+
+	$('#button1').click(function(){
+		$(".smallSection").hide();
+		$("#one").fadeIn("slow");
+	});
 
 	$('#button2').click(function(){
-		
+		$(".smallSection").hide();
+		$("#two").fadeIn("slow");
 	});
+
+	$('#button4').click(function(){
+		$(".smallSection").hide();
+		$("#three").fadeIn("slow");
+	});
+
+
 
 
 });
